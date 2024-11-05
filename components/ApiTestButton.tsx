a352641e45125c881f283757c79a6e3e43da3f1c
Clément <poudree.clement@gmail.com>
2024-11-05 17:57:49 +0100

    Fixed client component error

--- components/ApiTestButton.tsx
+++ components/ApiTestButton.tsx
@@ -1,10 +1,12 @@
+"use client";
+
 import axiosInstance from "../lib/axiosInstance";
 import { Button } from "@/components/ui/button";
 
 export const ApiTestButton = () => {
   const handleTest = async () => {
     try {
-      const response = await axiosInstance.get("");
+      const response = await axiosInstance.get("/");
       console.log(response);
     } catch (error) {
       console.error(error);
