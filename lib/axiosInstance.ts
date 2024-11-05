c6f6c1d10b70b718e22a4d572a06c92ae03fc0c8
Clément <poudree.clement@gmail.com>
2024-11-05 21:51:05 +0100

    Removed attempt to fix cors errors

--- lib/axiosInstance.ts
+++ lib/axiosInstance.ts
@@ -2,13 +2,6 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
-  headers: {
-    "Content-Type": "application/json",
-    "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-    "Access-Control-Allow-Headers": "X-Requested-With, Content-Type, Accept",
-    "Access-Control-Allow-Credentials": "true",
-  },
   withCredentials: true,
 });
 
